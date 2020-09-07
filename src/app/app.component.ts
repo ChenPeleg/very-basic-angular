459b292db715abbc80193797b0d967a9497c3e5e
Chen <Chenpeleg>
2020-09-07 10:13:09 +0300

    adding to webstorm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  private title = 'app';
-  public imgUrl1: string = "https://images.immediate.co.uk/production/volatile/sites/4/2009/07/GettyImages-931270318-43ab672.jpg";
-  public imgUrl2: string = "https://www.gravatar.com/avatar/d50c83cc0c6523b4d3f6085295c953e0";
-  private CORS: string = "https://cors-anywhere.herokuapp.com/";
-  @ViewChild('emptyIMG') emptyIMG;
+  title: string = "Date"
   constructor(private http: HttpClient) {
 
   }
@@ -22,42 +18,7 @@ export class AppComponent implements OnInit {
       headers: { 'Content-Type': 'image/jpg' }, params: {}, responseType: 'arraybuffer' as 'json'
     };
 
-    this.http.get<ArrayBuffer>(this.CORS + this.imgUrl2, options).subscribe(res => this.arrayBuffertoImage(res))
-
-  }
-
-  fetchAPI() {
-    fetch(this.imgUrl2).then(response => response.arrayBuffer()).then(buffer => this.arrayBuffertoImage(buffer))
-  }
-
-  arrayBuffertoImage(arrayBuffer: ArrayBuffer) {
-    var blob = new Blob([arrayBuffer], { type: "image/jpeg" });
-    var urlCreator = window.URL;
-    var imageUrl = window.URL.createObjectURL(blob);
-    this.emptyIMG.nativeElement.src = imageUrl;
   }
 
 
-
-
-  xhrConvertImageToDataURI() {
-    // function toDataURL(url, callback) {
-
-    //   var xhr = new XMLHttpRequest();
-    //   xhr.onload = function() {
-    //     var reader = new FileReader();
-    //     reader.onloadend = function() {
-    //       callback(reader.result);
-    //     }
-    //     reader.readAsDataURL(xhr.response);
-    //   };
-    //   xhr.open('GET', url);
-    //   xhr.responseType = 'blob';
-    //   xhr.send();
-    // }
-
-    // toDataURL('https://www.gravatar.com/avatar/d50c83cc0c6523b4d3f6085295c953e0', function(dataUrl) {
-    //   //console.log('RESULT:', dataUrl)
-    // })
-  }
 }
